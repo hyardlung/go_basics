2e0a862fc7ea50ac8bc5ff04f1058ac9030d3fc7
Dmitry Volkov <hyardlung@gmail.com>
2025-08-31 20:33:48 +0200

    Enhance ReadFile function: validate JSON file extension and content before reading

--- 3-struct/file/file.go
+++ 3-struct/file/file.go
@@ -1,13 +1,21 @@
 package file
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 func ReadFile(fileName string) ([]byte, error) {
 	data, err := os.ReadFile(fileName)
+	ext := filepath.Ext(fileName)
+	isValidJSON := json.Valid(data)
 	if err != nil {
+		return nil, err
+	}
+	if ext != ".json" || !isValidJSON {
+		err = fmt.Errorf("can't read %s file type. Only '.json' files available", ext)
 		return nil, err
 	}
 	return data, nil
